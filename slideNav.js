91f9ca071ebb8dda1e1a003efa41830007126220
Lawrence Aubert <lswaqqenonem@gmail.com>
2024-12-18 02:26:51 -0800

    Refining.

--- slideNav.js
+++ slideNav.js
@@ -1,17 +1,25 @@
+const bar = document.getElementById('three-bar')
+bar.style.fontWeight = '600'
+bar.style.fontSize = 'xx-large'
+bar.style.display = 'none'
+
 const nav = document.getElementById('link-container')
 let slide = false
 
-document.getElementById('three-bar').addEventListener('click', () => {
+bar.addEventListener('click', () => {
   slide = !slide
   nav.style.visibility = slide ? 'visible' : 'hidden'
 })
 
 function viewNav() {
   if (window.innerWidth <= 600 && !slide) {
+    bar.style.display = 'inline'
     nav.style.visibility = 'hidden'
   } else if (window.innerWidth <= 600 && slide) {
+    bar.style.display = 'inline'
     nav.style.visibility = 'visible'
   } else if (window.innerWidth > 600) {
+    bar.style.display = 'none'
     nav.style.visibility = 'visible'
   }
 }
